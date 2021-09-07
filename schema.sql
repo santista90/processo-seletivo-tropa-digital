935d8a5e1e4fcebba75652c242b40222b1851464
Luiz Felipe <luizfelipe90@hotmail.com>
2021-09-07 19:13:10 -0300

    Criado banco de dados e tabelas do projeto

---
+++ schema.sql
@@ -0,0 +1,25 @@
+create database tropadigital;
+
+create table if not exists usuarios (
+	id_usuario int not null,
+  	nome varchar(255) not null,
+  	sobrenome varchar(225) not null,
+  	email varchar(255) not null,
+	telefone varchar(55) not null,
+  	cpf varchar(55) not null,
+  	primary key (id_usuario)
+);
+
+create table if not exists endereco_usuarios (
+  	id_endereco_usuario int not null,
+  	id_usuario int not null,
+  	logradouro varchar(255) not null,
+  	numero varchar(45) not null, 
+  	cidade varchar(255) not null,
+  	uf varchar(2) not null,
+  	cep varchar(45) not null,
+  	bairro varchar(255) not null,
+  	complemento	varchar(255),
+  	primary key (id_endereco_usuario),
+  	foreign key (id_usuario) references usuarios(id_usuario)  
+);
