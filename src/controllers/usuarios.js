e158ac16ea17fc792ec88d1f9749208d5791f946
Luiz Felipe <luizfelipe90@hotmail.com>
2021-09-08 16:09:40 -0300

    Criado rota e função para listar usuarios

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,15 +1,15 @@
 const knex = require('../config');
+const schemaCadastrarUsuario = require('../validation/schemaCadastrarUsuario');
 
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, sobrenome, email, telefone, cpf } = req.body;
 
     try {
+        await schemaCadastrarUsuario.validate(req.body);
 
         const emailExistente = await knex('usuarios').where({ email }).first();
         const cpfExistente = await knex('usuarios').where({ cpf }).first();
-
-        console.log(emailExistente)
 
         if (emailExistente) {
             return res.status(400).json("O email já existe");
@@ -26,7 +26,24 @@ const cadastrarUsuario = async (req, res) => {
             return res.status(400).json("O Usuário não foi cadastrado.");
         }
 
-        return res.status(200).json('Usuário cadastrado com sucesso.');
+        return res.status(200).json("Ação Realizada com sucesso");
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+const listarUsuarios = async (req, res) => {
+
+    try {
+
+        const usuarios = await knex('usuarios');
+
+        if(usuarios.length === 0){
+            return res.status(400).json("Não possui usuarios cadastrados");
+        }
+
+        return res.status(200).json(usuarios);
 
     } catch (error) {
         return res.status(400).json(error.message);
@@ -35,5 +52,6 @@ const cadastrarUsuario = async (req, res) => {
 
 
 module.exports = {
-    cadastrarUsuario
+    cadastrarUsuario,
+    listarUsuarios
 }
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ const routes = express();
 
 // Cadastrar Usuário
 routes.post('/usuarios', usuarios.cadastrarUsuario);
+// Listar Usuários
+routes.get('/usuarios', usuarios.listarUsuarios);
 
 
 module.exports = routes;
