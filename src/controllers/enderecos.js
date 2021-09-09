04284debe1bb67d83a56a15ff390437fca537485
Luiz Felipe <luizfelipe90@hotmail.com>
2021-09-09 16:34:41 -0300

    corrigido bug

--- src/controllers/enderecos.js
+++ src/controllers/enderecos.js
@@ -16,7 +16,7 @@ const cadastrarEndereco = async (req, res) => {
         }
 
         
-        const endereco = await knex('endereco_usuarios').insert({ id_usuario, logradouro, numero, cidade, uf, cep, bairro, complemento });
+        const endereco = await knex('enderecos_usuario').insert({ id_usuario, logradouro, numero, cidade, uf, cep, bairro, complemento });
 
 
         if (endereco.length === 0) {
@@ -36,7 +36,7 @@ const listarEnderecos = async (req, res) => {
 
     try {
 
-        const enderecos = await knex('endereco_usuarios').where({ id_usuario });
+        const enderecos = await knex('enderecos_usuario').where({ id_usuario });
 
         if(enderecos.length === 0){
             return res.status(400).json("Não possui endereço cadastrado");
@@ -55,7 +55,7 @@ const obterEndereco = async (req, res) => {
 
     try {
 
-        const endereco = await knex('endereco_usuarios').where({ id_endereco_usuario });
+        const endereco = await knex('enderecos_usuario').where({ id_endereco_usuario });
 
         if(endereco.length === 0){
             return res.status(400).json("Endereço não encontrado");
@@ -73,7 +73,7 @@ const excluirEndereco = async (req, res) => {
 
     try {
 
-        const endereco = await knex('endereco_usuarios').where({ id_endereco_usuario }).del();
+        const endereco = await knex('enderecos_usuario').where({ id_endereco_usuario }).del();
 
         if(!endereco){
             return res.status(400).json("Endereço não encontrado");
@@ -92,18 +92,18 @@ const atualizarEndereco = async (req, res) => {
 
     try {
 
-        const enderecoEncontrado = await knex('endereco_usuarios').where({ id_endereco_usuario }).first();
+        const enderecoEncontrado = await knex('enderecos_usuario').where({ id_endereco_usuario }).first();
 
         if (!enderecoEncontrado) {
             return res.status(400).json("Endereço não existe");
         }
 
-        const usuarioEncontrado = await knex('endereco_usuarios').where({ id_usuario });
+        const usuarioEncontrado = await knex('enderecos_usuario').where({ id_usuario });
         
         if(usuarioEncontrado.length === 0){
             return res.status(400).json("Usuário não existe"); 
         } else {
-            const enderecoAtualizado = await knex('endereco_usuarios').where({ id_endereco_usuario }).update({ id_usuario, logradouro, numero, cidade, uf, cep, bairro, complemento });
+            const enderecoAtualizado = await knex('enderecos_usuario').where({ id_endereco_usuario }).update({ id_usuario, logradouro, numero, cidade, uf, cep, bairro, complemento });
 
             if (!enderecoAtualizado) {
                 return res.status(400).json("O Endereço não foi atualizado.");
