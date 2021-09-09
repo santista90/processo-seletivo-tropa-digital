7d45e3235d67eba144f7a7c0d69e08a4415d177a
Luiz Felipe <luizfelipe90@hotmail.com>
2021-09-09 16:44:43 -0300

    Atualizado rotas

--- src/routes.js
+++ src/routes.js
@@ -17,13 +17,13 @@ routes.put('/usuarios/:id_usuario', usuarios.atualizarUsuario);
 
 // Cadastrar Endereço
 routes.post('/enderecos-usuario', enderecos.cadastrarEndereco);
-// Listar Endereços
-routes.get('/enderecos-usuario/:id_usuario', enderecos.listarEnderecos);
-// Obter Endereços
-routes.get('/enderecos-usuario/:id_endereco_usuario', enderecos.obterEndereco);
 // Deletar Endereço
 routes.delete('/enderecos-usuario/:id_endereco_usuario', enderecos.excluirEndereco);
 // Atualizar Endereço
 routes.put('/enderecos-usuario/:id_endereco_usuario', enderecos.atualizarEndereco);
+// Listar Endereços
+routes.get('/enderecos-usuario/:id_usuario', enderecos.listarEnderecos);
+// Obter Endereços
+routes.get('/enderecos-usuario/:id_endereco_usuario', enderecos.obterEndereco);
 
 module.exports = routes;
